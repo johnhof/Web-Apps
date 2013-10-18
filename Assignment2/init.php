e1ed4bcdc1c7779314b1fd0773a4101f2793e04b
John Hofrichter <john.hofrichter@gmail.com>
2013-10-18 00:23:31 -0400

    table generation complete

--- Assignment2/init.php
+++ Assignment2/init.php
@@ -14,14 +14,37 @@
 
   if(!$db) exit;
 
-  $db->dropTable('test');
+//-------- DROPT OLD TABLES -------------------------------------------------------------------------------------------------------------
 
-  $db->createTable('test',['id int primary key not null',
-      						'name char(30) not null',
-      						'string char(50) not null']);
+  $db->dropTable('Makers');
+  $db->dropTable('Schedule');
+  $db->dropTable('Maker_Schedule');
+  $db->dropTable('Users');
+  $db->dropTable('Time');
 
-  $db->tableToString('test'); 
+//-------- GERNERATE TABLES -------------------------------------------------------------------------------------------------------------
+  
+  //MAKERS
+  $db->createTable('Makers',['email char(30) primary key not null',
+      							'name char(30) not null',
+      							'password char(50) not null']);
+  //SCHEDULE
+  $db->createTable('Schedule',['id int primary key not null',
+      							'name char(30) not null',
+      							'string char(50) not null']);
+  //MAKER_SCHECULE
+  $db->createTable('Maker_Schedule',['userId int not null',
+      						'scheduleID int not null',
+      						'primary key (userId, scheduleId)']);
+  //USERS
+  $db->createTable('Users',['id int primary key not null']);
+  //TIME
+  $db->createTable('Time',['userId int not null',
+      						'scheduleId int not null',
+      						'dateTime char(40) not null',
+      						'primary key (userId, scheduleId, dateTime)']);
 
+//-------- ADD TUPLES -------------------------------------------------------------------------------------------------------------------
 
 
 ?>
